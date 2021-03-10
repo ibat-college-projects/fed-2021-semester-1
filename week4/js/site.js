1e54de36570cf35b5e7991e3636ca20de6abdc92
John Rowley <outsidethebox@eircom.net>
2021-03-10 19:32:18 +0000

    clear and loading

--- week4/js/site.js
+++ week4/js/site.js
@@ -23,9 +23,34 @@ function onSelectChange(e) {
 function clearItems() {
     console.log(`clearItems() is called`);
 
+    // similar to document.getElementById - we use # to get by id, and a dot (.) to retrieve by class
+  //  const selectedObject = document.querySelector('#myListOfCities');
+    const selectedObject = document.querySelector('.cityListing');
+
+    selectedObject.options.length =0;
+
 }
 
 function loadItems() {
     console.log(`loadItems() is called`);
+
+    // Choosing by # to illustrate different techniques
+    const selectedObject = document.querySelector('#myListOfCities');
+
+    let option1 = document.createElement("option");
+    let option2 = document.createElement("option");
+
+    option1.value = '300';
+    option1.text = 'Limerick';
+
+    option2.value = '400';
+    option2.text = 'Galway';
+  
+    selectedObject.add(option1,null);
+    selectedObject.add(option2,null);
+
+    let countItems = selectedObject.options.length;
+
+    console.log(`Total Items is ${countItems}`);
 
 }
