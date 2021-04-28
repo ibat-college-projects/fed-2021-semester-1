8a574c2361ba3c9daa45f4bb3e49bee8ff5f63e8
John Rowley <outsidethebox@eircom.net>
2021-04-28 20:33:48 +0100

    added storage array splice options

--- week9/js/site.js
+++ week9/js/site.js
@@ -2,6 +2,19 @@
 let fleet = []
 
 function init() {
+
+    const fleetString = localStorage.getItem("fleet");
+
+    if (fleetString === undefined || fleetString === null) {
+        fleet = [];
+        return;
+    }
+
+    fleet = JSON.parse(fleetString);
+
+
+    renderData(fleet);
+
 }
 
 
@@ -12,16 +25,18 @@ function btnSave() {
     carObject.model = document.getElementById("tbCarModel").value;
     carObject.engine = document.getElementById("tbCarEngine").value;
 
-    console.log({carObject});
+    console.log({ carObject });
 
-    fleet.push(carObject)    
+    fleet.push(carObject)
 
     const carObjectAsString = JSON.stringify(fleet);
-    localStorage.setItem("fleet",carObjectAsString)
+    localStorage.setItem("fleet", carObjectAsString)
+
+    renderData(fleet);
 
 }
 
-function btnRetrieve () {
+function btnRetrieve() {
 
     const carDataAsString = localStorage.getItem("fleet");
 
@@ -33,4 +48,36 @@ function btnRetrieve () {
     const fleetAsObject = JSON.parse(carDataAsString);
 
     console.log(fleetAsObject);
+}
+
+function renderData(fleet) {
+
+    let listingDiv = document.getElementById("tbodyCarData")
+
+    listingDiv.innerHTML = '';
+    htmlString = [];
+
+    for(const [index, currentCar] of fleet.entries()) {
+
+        console.log(`${index} ${currentCar.mf} ${currentCar.model} ${currentCar.engine}`);
+        const removeButton = `<button class='btn  btn-primary' onclick='removeCar(${index})'>Remove ${index}</button>`;
+        const rowItem = `<tr><td>${currentCar.mf}</td><td>${currentCar.model}</td><td>${currentCar.engine}</td><td>${removeButton}</td></tr>`
+
+        htmlString.push(rowItem);
+
+    }
+
+    listingDiv.innerHTML = htmlString.join('');
+}
+
+function removeCar(indexOfCar) {
+
+    console.log(`REmoving ${indexOfCar}`);
+
+    fleet.splice(indexOfCar,1);
+
+    localStorage.setItem("fleet",JSON.stringify(fleet));
+
+    renderData(fleet);
+
 }
